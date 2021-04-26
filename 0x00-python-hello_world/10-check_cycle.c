ffa124bc51d2b09b3a703b598a716a1286631b0d
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-04-26 18:05:52 -0500

    [Betty styles]

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,9 +1,15 @@
 #include "lists.h"
 #include <string.h>
 #include <stdio.h>
+/**
+ *check_cycle - Check a circule list
+ *@list: header of the list
+ *Return: 1 or 0 if is or isn't a cicrcule list
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *aux= list;
+	listint_t *aux = list;
+
 	list = list->next;
 	while (list != NULL)
 	{
