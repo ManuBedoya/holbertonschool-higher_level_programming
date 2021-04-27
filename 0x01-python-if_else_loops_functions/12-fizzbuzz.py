cda4d0e41d485eca067da5f3eddec3736488c2a9
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-04-27 15:06:37 -0500

    [fizzBuzz] Done!

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        text = i
+        if (i % 3 == 0 and i % 5 == 0):
+            text = "FizzBuzz"
+        elif (i % 3 == 0):
+            text = "Fizz"
+        elif (i % 5 == 0):
+            text = "Buzz"
+        if (i == 100):
+            print("{}".format(text))
+        else:
+            print("{}".format(text), end=' ')
