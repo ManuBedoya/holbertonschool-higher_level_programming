cd6c777c397106916c5cb817a931f04021c01861
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-04-28 13:53:59 -0500

    [args] Done!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+if (len(sys.argv) == 1):
+    print("0 arguments.")
+elif (len(sys.argv) == 2):
+    print("1 argument:\n1: {}".format(sys.argv[1]))
+else:
+    print("{:d} arguments:".format(len(sys.argv)))
+    for i in range(2, len(sys.argv) + 1):
+        print("{:d}: {}".format(i, sys.argv[i - 1]))
