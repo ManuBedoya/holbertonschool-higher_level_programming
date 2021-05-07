195910f54ced4c1369cccce82de47d4d6317e376
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-05-07 14:54:09 -0500

    [RomanToInt] Whith less too

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,13 @@ def roman_to_int(roman_string):
         romanNumber = 0
         valuesRoman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                        'C': 100, 'D': 500, 'M': 1000}
-        for letter in roman_string:
-            romanNumber += valuesRoman.get(letter)
+        for i in range(len(roman_string)):
+            valueTemp = valuesRoman.get(roman_string[i])
+            if (i == (len(roman_string) - 1)):
+                romanNumber += valueTemp
+            else:
+                if valuesRoman.get(roman_string[i + 1]) > valueTemp:
+                    romanNumber -= valueTemp
+                else:
+                    romanNumber += valueTemp
         return romanNumber
