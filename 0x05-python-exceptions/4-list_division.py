292f7e984774b73d33d640c388c0dc992e902d9b
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-05-17 12:57:16 -0500

    [PrintDivision] update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     newList = []
     iterators = 0
-    if (len(my_list_1) > len(my_list_2)):
+    if (my_list_1 < my_list_2):
         iterators = len(my_list_1)
     else:
         iterators = len(my_list_2)
