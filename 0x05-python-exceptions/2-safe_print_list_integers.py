00a6ac75c7963597db4676c02c98dadbcb6f40b2
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-05-17 12:42:21 -0500

    [PrintListInteger] Fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    if not x or not my_list:
-        return count
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
