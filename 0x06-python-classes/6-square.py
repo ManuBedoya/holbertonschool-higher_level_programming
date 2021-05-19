d8223aac11f538526d6de6bcdf280f21663a310a
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-05-19 12:17:45 -0500

    [6Square] update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -82,8 +82,8 @@ class Square:
         if (self.__size == 0):
             print()
 
+        for i in range(self.__position[1]):
+                print()
         for row in range(self.__size):
             print(" " * self.__position[0], end="")
-            print("#" * self.__size, end="")
-            if (self.__position[1] > 0):
-                print()
+            print("#" * self.__size)
