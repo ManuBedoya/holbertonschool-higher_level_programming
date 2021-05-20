820f38c9a2b5b2cbec5482921b5035c8d38b0f0f
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-05-20 12:56:11 -0500

    [All] Up

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module to create a Reactangle Object
+"""
+
+
+class Rectangle:
+    """This is a Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        """This is __init__ method
+        Is to initianilize the variables
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
