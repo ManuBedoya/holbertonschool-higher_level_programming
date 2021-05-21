26193796edfd2e43161ab55fb1f65dcc3cde79f3
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-05-20 19:20:41 -0500

    [1Rectangle] update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
         """This is __init__ method
         Is to initianilize the variables
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
