324a0a5c1eae0442e874be8d2271662b6ddb0b46
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-05-31 17:40:27 -0500

    [appendWrite] update

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""append and write a file"""
+
+
 def append_write(filename='', text=''):
-    with open(filename, 'a+') as file:
-        number_chars = file.write(text)
-    file.close()
-    return number_chars
+    """This is the append_write method"""
+    with open(filename, 'a+', encoding='utf-8') as file:
+        return file.write(text)
