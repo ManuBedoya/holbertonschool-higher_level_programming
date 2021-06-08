15ad63fba2b286ef178174c138389c8bea06a976
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-06-08 15:52:00 -0500

    [tests] update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,7 +7,7 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     """ Test the base class """
 
-    def test_init(self):
+    def test_without_arguments(self):
         """ Test cases of normal initialization """
         b1 = Base()
         self.assertEqual(b1.id, 1)
@@ -18,8 +18,25 @@ class TestBase(unittest.TestCase):
         b3 = Base()
         self.assertEqual(b3.id, 3)
 
-        b4 = Base(12)
-        self.assertEqual(b4.id, 12)
-
         b4 = Base()
         self.assertEqual(b4.id, 4)
+
+        b4 = Base()
+        self.assertEqual(b4.id, 5)
+
+    def test_with_arguments(self):
+        """ Test cases of normal initialization """
+        b1 = Base(8)
+        self.assertEqual(b1.id, 8)
+
+        b2 = Base(4)
+        self.assertEqual(b2.id, 4)
+
+        b3 = Base(1)
+        self.assertEqual(b3.id, 1)
+
+        b4 = Base(25)
+        self.assertEqual(b4.id, 25)
+
+        b4 = Base(123)
+        self.assertEqual(b4.id, 123)
