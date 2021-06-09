3e57d8413c091548977fef9e4dcbefc18d8e1ae4
Manu Bedoya <manuelfernandobedoya@gmail.com>
2021-06-09 11:08:56 -0500

    [tests] update

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -17,3 +17,46 @@ class TestRectangle(unittest.TestCase):
 
         r2 = Rectangle(10, 2, 0, 0, 23)
         self.assertEqual(r2.height, 2)
+
+    def test_error_types(self):
+        """Test of errors
+        """
+        try:
+            r1 = Rectangle(1, '2')
+        except Exception as err:
+            pass
+        self.assertRaises(TypeError)
+
+        try:
+            r2 = Rectangle('1', 2)
+        except Exception as err:
+            pass
+        self.assertRaises(TypeError)
+
+    def test_error_values(self):
+        """Test with arguments negatives
+        """
+        try:
+            r1 = Rectangle(-10, 6)
+        except Exception as err:
+            pass
+        self.assertRaises(ValueError)
+
+        try:
+            r1 = Rectangle(8, -6)
+        except Exception as err:
+            pass
+        self.assertRaises(ValueError)
+
+
+        try:
+            r1 = Rectangle(0, 6)
+        except Exception as err:
+            pass
+        self.assertRaises(ValueError)
+
+        try:
+            r1 = Rectangle(8, 0)
+        except Exception as err:
+            pass
+        self.assertRaises(ValueError)
