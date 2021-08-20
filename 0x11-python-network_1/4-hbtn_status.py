3cc068f06e688e812c693236f063976165882648
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-20 17:23:38 -0500

    [hbtn_status] Fix!

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,7 @@ import requests
 
 if __name__ == '__main__':
     r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\ttype: {}\n\
-    \tcontent: {}".format(type(str(r)), r.content.decode("utf8")))
+
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.content.decode("utf8")))
