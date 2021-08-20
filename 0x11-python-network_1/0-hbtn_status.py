f28b0e73c9ece7d234e07ef98ddc51cb25e77cc6
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-20 17:13:34 -0500

    [hbtn_status] Fix the msg!

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,7 @@ import urllib.request
 
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        is_utf8 = ""
-        verify = response.headers["Content-Type"].split("; ")
-        if verify[1] == "charset=utf-8":
-            is_utf8 = "OK"
         res = response.read()
+
         print("Body response:\n\t- type: {}\n\t- content: {}\n\
-        - utf8 content: {}".format(type(res), res, is_utf8))
+        - utf8 content: {}".format(type(res), res, res.decode("utf8")))
