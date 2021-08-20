fea2c4c50bf7c51367f5d1ef1c9eb3dfdf441a7e
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-20 15:14:21 -0500

    [erroe_code] Fix the errors!

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,7 +5,8 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    try:
-        print(requests.get(sys.argv[1]).content.decode("utf8"))
-    except HTTPError as err:
-        print("Error code: {}".format(err))
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.content.decode("utf8"))
