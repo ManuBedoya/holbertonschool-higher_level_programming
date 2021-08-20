f8af5438ccd8ed4319f5807bc8c12b93f942849d
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-20 13:57:28 -0500

    [hbtn_status] Done!

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module to Status
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:\n\ttype: {}\n\
+    \tcontent: {}".format(type(str(r)), r.content.decode("utf8")))
