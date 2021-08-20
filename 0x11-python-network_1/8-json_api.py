d0a01ca5e204cf22cfe5c39bffc7ab6baaafa5b7
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-20 15:41:07 -0500

    [json_api] more arguments ignored

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,7 @@ import sys
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
+    if len(sys.argv) >= 2:
         values = {"q": sys.argv[1]}
         r = requests.post('http://0.0.0.0:5000/search_user', data=values)
         if r.text == "{}\n":
