3a8d34f05435c3dfc0ad67f31af628a80ffe825d
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-20 15:39:10 -0500

    [json_api] Done!

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module to take id  and name with letter
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        values = {"q": sys.argv[1]}
+        r = requests.post('http://0.0.0.0:5000/search_user', data=values)
+        if r.text == "{}\n":
+            print("No result")
+        else:
+            catch_var = r.text.split(":")
+            print("[{}] {}".format(catch_var[1].split(",")[0],
+                                   catch_var[2][1:-3]))
+    else:
+        print("No result")
