d13a0d80936c60235c0e038e54452c1715082b2f
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-20 17:16:13 -0500

    [hbtn_status] Fix the msg!

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
         res = response.read()
 
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode('utf-8')))
