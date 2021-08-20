fedfa54b93aae8d9d57e78ad31b1726203c49225
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-08-20 17:30:57 -0500

    [json_api] Completing task!

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,8 +6,12 @@ import sys
 
 
 if __name__ == '__main__':
-    if len(sys.argv) >= 2:
+    if bool(sys.argv[1]):
         values = {"q": sys.argv[1]}
+    else:
+        values = {"q": ""}
+
+    try:
         r = requests.post('http://0.0.0.0:5000/search_user', data=values)
         if r.text == "{}\n":
             print("No result")
@@ -15,5 +19,5 @@ if __name__ == '__main__':
             catch_var = r.text.split(":")
             print("[{}] {}".format(catch_var[1].split(",")[0],
                                    catch_var[2][1:-3]))
-    else:
-        print("No result")
+    except ValueError:
+        print("Not a valid JSON")
