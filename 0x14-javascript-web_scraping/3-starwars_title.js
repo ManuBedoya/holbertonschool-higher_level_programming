d15de670b5794c6c5459eae8bad2442cfee81c99
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-09-14 13:10:13 -0500

    styles!

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,32 +1,29 @@
 #!/usr/bin/node
-const https = require('https')
+const https = require('https');
 
-let count_slash = 0;
-let before = '';
-let after = '';
-let url = "swapi-api.hbtn.io"
-let path = `/api/films/${process.argv[2]}/`
+const url = 'swapi-api.hbtn.io';
+const path = `/api/films/${process.argv[2]}/`;
 const options = {
   hostname: url,
   port: 443,
   path: path,
   method: 'GET'
-}
+};
 
 const req = https.request(options, res => {
-    let todo = ''
-    res.on('data', (chunk) => {
-	todo += chunk;
-    });
+  let todo = '';
+  res.on('data', (chunk) => {
+    todo += chunk;
+  });
 
   // called when the complete response is received.
-    res.on('end', () => {
-	console.log(JSON.parse(todo).title);
-    });
-})
+  res.on('end', () => {
+    console.log(JSON.parse(todo).title);
+  });
+});
 
 req.on('error', error => {
-  console.error(error)
-})
+  console.error(error);
+});
 
-req.end()
+req.end();
