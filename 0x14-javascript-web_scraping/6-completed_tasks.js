76add923afe8f03d5a096094464ed21103712b53
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-09-15 08:53:51 -0500

    [completed_task] Semistandard!

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,14 +5,16 @@ const users = {};
 
 request(process.argv[2], function (error, response, body) {
   const array = JSON.parse(body);
-  const numberUsers = array[array.length - 1].userId;
-  for (let i = 1; i <= numberUsers; i++) {
-    users[i] = 0;
-  }
-  if (error) { console.log(error); }
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].completed) {
-      users[array[i].userId]++;
+  if (array.length !== 0) {
+    const numberUsers = array[array.length - 1].userId;
+    for (let i = 1; i <= numberUsers; i++) {
+      users[i] = 0;
+    }
+    if (error) { console.log(error); }
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].completed) {
+        users[array[i].userId]++;
+      }
     }
   }
   console.log(users);
