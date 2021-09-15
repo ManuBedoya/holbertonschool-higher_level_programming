6642a995b970d5a3997db8dac3d83c28ff064d6c
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-09-14 23:13:25 -0500

    [statuscode] with request!

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,33 +1,7 @@
 #!/usr/bin/node
 // const protocol = require('https')
-const https = require('https');
+const request = require('request');
 
-let countSlash = 0;
-let before = '';
-let after = '';
-for (let i = 0; i < process.argv[2].length; i++) {
-  if (process.argv[2][i] === '/') {
-    countSlash++;
-  }
-  if (countSlash >= 3) {
-    after += process.argv[2][i];
-  } else if (countSlash === 2 && process.argv[2][i] !== '/') {
-    before += process.argv[2][i];
-  }
-}
-const options = {
-  hostname: before,
-  port: 443,
-  path: after,
-  method: 'GET'
-};
-
-const req = https.request(options, res => {
-  console.log(`code: ${res.statusCode}`);
-});
-
-req.on('error', error => {
-  console.error(error);
-});
-
-req.end();
+request(process.argv[2], function(error, response, body){
+    console.log("code:", response.statusCode);
+})
