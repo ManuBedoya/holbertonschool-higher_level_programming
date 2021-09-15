358703e53c748f5b413c44e8ffe66d20f9208040
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-09-15 00:34:12 -0500

    [advanced] Done!

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const GET = request.get;
 
-const number = process.argv[2];
+
+const num = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + number;
 
-GET(url, (err, res, body) => {
+request.get(url, (err, res, body) => {
   if (err) return (console.log(err));
   const movies = JSON.parse(body);
 
-  movies.characters.forEach(charUrl => GET(charUrl, (err, res, body) => {
+  movies.characters.forEach(charUrl => request.get(charUrl, (err, res, body) => {
     if (err) return (console.log(err));
     console.log(JSON.parse(body).name);
   }));
